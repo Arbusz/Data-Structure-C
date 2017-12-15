310860b74dab18c3549cb97bcf0350696eb8344f
vimrc <arbuszg@gmail.com>
2017-12-15 15:50:58 +0800

    图的深度优先遍历

---
+++ DFS.cpp
@@ -0,0 +1,35 @@
+//
+// Created by arbus on 2017/12/13.
+//
+
+#include <iostream>
+#define N 5
+using namespace std;
+int matrix[N][N] = {
+        { 0, 1, 1, 0, 0 },
+        { 0, 0, 1, 0, 1 },
+        { 0, 0, 1, 0, 0 },
+        { 1, 1, 0, 0, 1 },
+        { 0, 0, 1, 0, 0 }
+};
+int visited[N + 1] = { 0, };
+void DFS(int start)
+{
+    visited[start] = 1;
+    for (int i = 1; i <= N; i++)
+    {
+        if (!visited[i] && matrix[start - 1][i - 1] == 1)
+            DFS(i);
+    }
+    cout << start << " ";
+}
+int main()
+{
+    for (int i = 1; i <= N; i++)
+    {
+        if (visited[i] == 1)
+            continue;
+        DFS(i);
+    }
+    return 0;
+}
